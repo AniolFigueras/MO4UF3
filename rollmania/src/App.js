8122242b2d514749e030e6a9b39bf7215efda6f7
Aniol Figueras <aniol.figueras@enti.cat>
2025-02-19 11:21:19 +0100

    Creacion rollmania de React

---
+++ rollmania/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Title from './Title.js';
+import Die from './Die.js';
+
+
+function App() {
+  return (
+		
+	<div className="rollmania">
+		<Title />
+    	<main className="App">
+				<Die />
+				<Die />
+				<Die />
+    	</main>
+		</div>
+  );
+}
+
+export default App;
